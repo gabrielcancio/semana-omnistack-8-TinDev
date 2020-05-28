b86160bcaf2c9454475f8a8eb3232f9663e57492
gabrielcancio <gabriel.cancio02@gmail.com>
2020-05-27 22:50:54 -0300

    Making the route system

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    response.json({
-        evento: 'Semama OmniStack',
-        aluno: 'Gabriel Cancio',
-        versao: 8.0
-    })
-});
+routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
+
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
